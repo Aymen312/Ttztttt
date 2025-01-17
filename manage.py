6345777203cd62194953d65e4e752f8921f79261
Aymen312 <153440336+Aymen312@users.noreply.github.com>
2025-01-17 22:35:52 +0100

    Update manage.py

--- manage.py
+++ manage.py
@@ -169,6 +169,27 @@ if uploaded_file is not None:
                         st.dataframe(somme_qte_par_date)
                 else:
                     st.warning("Aucune donnée trouvée pour cette désignation.")
+
+            # Nouvelle fonctionnalité : Recherche par taille
+            st.write("---")  # Une ligne de séparation
+            st.subheader("Recherche par taille")
+
+            # Zone de texte pour saisir une taille
+            st.markdown("<p style='color: var(--primary-color);'>Entrez une taille pour voir les quantités réalisées et les désignations correspondantes :</p>", unsafe_allow_html=True)
+            taille_recherchee = st.text_input("Taille")
+
+            if taille_recherchee:
+                # Filtrer les données pour la taille spécifiée et exclure qte_rel == 0
+                data_taille = data[(data['taille'].astype(str).str.contains(taille_recherchee, case=False, na=False)) &
+                                   (data['qte_rel'] != 0)]
+
+                if not data_taille.empty:
+                    # Afficher les résultats (designation et qte_rel)
+                    resultats_taille = data_taille[['designation', 'qte_rel']]
+                    st.write(f"Résultats pour la taille '{taille_recherchee}' :")
+                    st.dataframe(resultats_taille)
+                else:
+                    st.warning("Aucune donnée trouvée pour cette taille.")
         else:
             st.error("La colonne 'datelivraison' n'existe pas dans le fichier CSV.")
     except Exception as e:
